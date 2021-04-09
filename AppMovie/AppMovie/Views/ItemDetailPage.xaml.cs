87bd00026a90f8f4caff543fbea5c0511bfbe10d
Antonio Restrepo <jheanpiertony@gmail.com>
2021-04-09 13:58:21 -0500

    UpDate Xamarin App

---
+++ AppMovie/AppMovie/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppMovie.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppMovie.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppMovie/AppMovie/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppMovie.Models;
+using AppMovie.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMovie.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
